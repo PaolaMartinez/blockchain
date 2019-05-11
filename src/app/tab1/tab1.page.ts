987b019fa7bb054f0cb5ccc8489b801fa6b856b1
PaolaMartinez <pao-mar96@hotmail.com>
2019-05-11 18:05:42 -0500

    cambios tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  showed = false;
+  showed2 = false;
+
+
+  infoButtonClick(){
+    this.showed = !this.showed;
+  }
+
+  infoButtonClick2(){
+    this.showed2 = !this.showed2;
+  }
+}

d77ae6b77181bfeadaa5a3aa5a014b3024de19c2
hug0er <hugo.e.betancourt@gmail.com>
2019-05-13 17:57:12 -0500

    cambios

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {LoadingController} from '@ionic/angular'
 import { forkJoin } from 'rxjs';
 import * as crypto from "crypto-js"; 
+import { promise } from 'protractor';
+
 
 const apiUrl = "172.23.203.14";
 
@@ -18,25 +21,43 @@ export class Tab2Page {
 registrado = false;
 clave = undefined;
 Hash = "vacio"
+Hash2 = []
 body = {"mensaje" : "hola"}
+estado = "Ganador indefinido"
 
-constructor(private http:HttpClient){
+constructor(private http:HttpClient, private zone : NgZone,private paco : LoadingController){
 
 
 }
+
+
 register(){
-  this.getData()
+  let controlador = this.paco.create({
+    message: "Please wait ..."
+  });
+  controlador.then((res2) => {
+    res2.present()
+    this.getData()
     .subscribe(res => {
         console.log(res)
         console.log(res[0].lastBlock)
         this.registrado = true
         this.proofOfWork(res[0].lastBlock.previous_hash)
-      
+        res2.dismiss().then(dis => {
+          console.log("dis")
+        })
       //loading.dismiss();
     }, err => {
       console.log(err);
+      res2.dismiss().then(dis => {
+        console.log("dis")
+      })
       //loading.dismiss();
     });
+
+
+  })
+  
   
 }
 
@@ -58,29 +79,53 @@ async getData2() {
 }
 
 getData(): Observable<any> {
-  let response1 = this.http.post('http://172.23.206.14:5000'+'/nodes/register',this.body);
+  let response1 = this.http.post('http://192.168.10.101:5000'+'/nodes/register',this.body);
   return forkJoin([response1]);
 }
 
 proofOfWork(lastProof) {
-  let proof = 0;
+  let proof = Math.floor(Math.random()*1000000);
   while (!this.validProof(lastProof, proof)) {
-    console.log(proof)
     proof += 1;
   }
-  let response1 = this.http.post('http://172.23.206.14:5000'+'/ganador',{"Hash" : this.Hash})
-  return proof;
+  console.log("entre")
+  this.http.post('http://192.168.10.101:5000'+'/ganador',{"Hash" : this.Hash}).subscribe((res : any) => {
+  console.log(res)
+  this.estado = res.message  
+}, err => {
+  console.log(err);
+});
+
+  console.log("sali")
 }
 
-validProof(lastProof, proof) {
+  validProof(lastProof, proof) {
   let guess = `${lastProof}${proof}`;
-  let vacio = setTimeout(()=> {
-    
-  },1000)
   let guessHash = crypto.SHA256(guess).toString(crypto.enc.Hex);
-  this.Hash = guessHash;
-  return guessHash.slice(0, 2) === '00';
+  console.log(guessHash)
+
+    this.Hash = guessHash
+    let Hash1 =  guessHash.slice(0,31);
+    let Hash2 =  guessHash.slice(32,64);
+    this.Hash2.push(Hash1 + '\n' + Hash2)
+    console.log("Intento Actualizarme")
+  
+  
+  return guessHash.slice(0, 3) === '000';
+
 }
+
+
+delayResult (ms) {
+  var start = new Date().getTime()
+  var end = start + ms
+  while (start < end){
+    start = new Date().getTime()
+  }
+
+}
+
+
 
 
 }
